3635a836a7ebdfcdbca882808a40d9a1a10be261
Jayden Asante <jja33@kent.ac.uk>
2024-10-15 19:56:34 +0100

    Completed Part One

--- partOne.py
+++ partOne.py
@@ -1,1 +1,10 @@
+def main():
+    age = int(input("What is your age? "))
+    alt(age)
 
+def alt(age):
+    if age >= 18:
+        print("Your are an adult")
+    elif age < 18:
+        print("You are a child!")
+main()
