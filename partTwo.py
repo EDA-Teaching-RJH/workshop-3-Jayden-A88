a69e069fb3b788e4288ef77cc812d4b876f28389
Jayden Asante <jja33@kent.ac.uk>
2024-10-15 21:11:57 +0100

    Completed Part Two

--- partTwo.py
+++ partTwo.py
@@ -1,1 +1,17 @@
+import random
 
+def main():
+    val = int(input("Pick a Number between 1 and 10: "))
+    myst(val)
+    
+def myst(val):
+    sn = random.randint(1, 10)
+    if val > sn:
+        print("Too High")
+    elif val < sn:
+        print("Too Low")
+    elif val == sn:
+        print("Correct!")   
+    print(f"The number was: {sn}")
+
+main()
