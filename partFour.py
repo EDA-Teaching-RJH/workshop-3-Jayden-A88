f2ca593b50a443cad36fffcae0756a4be2bcfbd2
Jayden Asante <jja33@kent.ac.uk>
2024-10-16 01:24:56 +0100

    completed

--- partFour.py
+++ partFour.py
@@ -1,1 +1,14 @@
+def main():
+    AD = "admin"
+    PS = "password123"
+    ADI = input("Username: ")
+    PSI = input("Password: ")
+    verify(AD, PS, PSI, ADI)
 
+def verify(AD, PS, PSI, ADI):
+    if ADI == AD and PS == PSI:
+         print("Access Granted")
+    else:
+        print("Error Access Denied")
+
+main()
