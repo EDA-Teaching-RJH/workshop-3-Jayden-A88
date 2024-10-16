85758e996552f9b57ee93617e37ffcba4fa4df6c
Jayden Asante <jja33@kent.ac.uk>
2024-10-16 12:01:38 +0100

    completed

--- partFive.py
+++ partFive.py
@@ -1,1 +1,12 @@
+def main():
+    Weekend = {"Saturday", "Sunday"}
+    Day = input("What is the current day of the week? ")
+    con(Weekend, Day)
+    
+def con(Weekend, Day):
+    if Day in Weekend:
+        print("It's the weekend!")
+    else:
+        print("It's a weekday.")
 
+main()
