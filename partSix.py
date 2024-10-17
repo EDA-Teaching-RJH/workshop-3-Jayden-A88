84bfd656e2c39c8c7332fdd17b86825c0e1083c6
Jayden Asante <jja33@kent.ac.uk>
2024-10-17 17:50:35 +0100

    completed

--- partSix.py
+++ partSix.py
@@ -1,1 +1,20 @@
+def main():
+    A = range(0, 11)
+    B = range(65, 1000000)
+    C = range(12, 64)
+    ST = input("Are you a student? ").strip().lower()
+    Age = int(input("What is your age? "))
+    DC(A, B, C, ST, Age)
 
+def DC(A, B, C, ST, Age):
+    if Age in A and ST == "no":
+        print("Ticket is £5")
+    elif Age in B and ST == "no":
+        print("Ticket is £5")
+    elif Age in C and ST == "yes":
+        print("Ticket is £8")
+    elif Age in C and ST == "no":
+        print("Ticket is £10")
+    else:
+        print("Error")
+main()
